64b49da485db336d02315f858e1c6a87b9c39a6d
ikuma <namashin.xxx@icloud.com>
2024-02-08 02:37:51 +0800

    add test

--- main_test.go
+++ main_test.go
@@ -24,11 +24,11 @@ var TestUser = dto.SignUpInput{
 }
 
 func TestMain(m *testing.M) {
+	// open test env
 	err := godotenv.Load(".env.test")
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	os.Exit(m.Run())
 }
 
@@ -46,10 +46,12 @@ func setUp() *gin.Engine {
 // _signUp method for Test User
 func _signUp(t *testing.T, router *gin.Engine, signUpTestUser dto.SignUpInput) dto.SignUpInput {
 	// sign up request body
-	signUpReqBody, _ := json.Marshal(signUpTestUser)
+	reqBody, err := json.Marshal(signUpTestUser)
+	assert.Equal(t, err, nil)
 
 	// http request
-	signUpReq, _ := http.NewRequest(http.MethodPost, "/auth/signup", bytes.NewBuffer(signUpReqBody))
+	signUpReq, err := http.NewRequest(http.MethodPost, "/auth/signup", bytes.NewBuffer(reqBody))
+	assert.Equal(t, err, nil)
 
 	// http response
 	w := httptest.NewRecorder()
@@ -60,20 +62,23 @@ func _signUp(t *testing.T, router *gin.Engine, signUpTestUser dto.SignUpInput) d
 	// ? signup succeeded
 	assert.Equal(t, http.StatusCreated, w.Code)
 
-	// get response new user data
-	var signUpRespBody map[string]dto.SignUpInput
-	_ = json.Unmarshal(w.Body.Bytes(), &signUpRespBody)
+	// put http response result in resBody
+	var resBody map[string]dto.SignUpInput
+	err = json.Unmarshal(w.Body.Bytes(), &resBody)
+	assert.Equal(t, err, nil)
 
-	return signUpRespBody["user"]
+	return resBody["user"]
 }
 
 // _login method for Test User to login
 func _login(t *testing.T, router *gin.Engine, loginUser dto.LoginInput) string {
 	// request body
-	loginReqBody, _ := json.Marshal(loginUser)
+	reqBody, err := json.Marshal(loginUser)
+	assert.Equal(t, err, nil)
 
 	// http request
-	req, _ := http.NewRequest(http.MethodPost, "/auth/login", bytes.NewBuffer(loginReqBody))
+	req, err := http.NewRequest(http.MethodPost, "/auth/login", bytes.NewBuffer(reqBody))
+	assert.Equal(t, err, nil)
 
 	// http response
 	w := httptest.NewRecorder()
@@ -84,11 +89,12 @@ func _login(t *testing.T, router *gin.Engine, loginUser dto.LoginInput) string {
 	// ? login succeeded
 	assert.Equal(t, http.StatusOK, w.Code)
 
-	// get response token data
-	var loginRespBody map[string]string
-	_ = json.Unmarshal(w.Body.Bytes(), &loginRespBody)
+	// put http response result in resBody
+	var resBody map[string]string
+	err = json.Unmarshal(w.Body.Bytes(), &resBody)
+	assert.Equal(t, err, nil)
 
-	return loginRespBody["token"]
+	return resBody["token"]
 }
 
 // _createTestItems make count numbers item
@@ -103,10 +109,12 @@ func _createTestItems(t *testing.T, router *gin.Engine, token string, count int)
 		}
 
 		// http request body
-		reqBody, _ := json.Marshal(newItem)
+		reqBody, err := json.Marshal(newItem)
+		assert.Equal(t, err, nil)
 
 		// http request
-		req, _ := http.NewRequest(http.MethodPost, "/items", bytes.NewBuffer(reqBody))
+		req, err := http.NewRequest(http.MethodPost, "/items", bytes.NewBuffer(reqBody))
+		assert.Equal(t, err, nil)
 
 		// set token in header
 		req.Header.Set("Authorization", "Bearer "+token)
@@ -120,11 +128,12 @@ func _createTestItems(t *testing.T, router *gin.Engine, token string, count int)
 		// ? new item is created successfully
 		assert.Equal(t, http.StatusCreated, w.Code)
 
-		// save new created item ID
-		var resData map[string]models.Item
-		_ = json.Unmarshal(w.Body.Bytes(), &resData)
+		// put http response result in resBody
+		var resBody map[string]models.Item
+		err = json.Unmarshal(w.Body.Bytes(), &resBody)
+		assert.Equal(t, err, nil)
 
-		createdItemIDs = append(createdItemIDs, resData["data"].ID)
+		createdItemIDs = append(createdItemIDs, resBody["data"].ID)
 	}
 
 	return createdItemIDs
@@ -144,50 +153,54 @@ func _findMyAll(t *testing.T, router *gin.Engine, token string) []models.Item {
 	// do API FindMyAll request
 	router.ServeHTTP(w, req)
 
-	// put http response result in res
-	var myItems map[string][]models.Item
-	err = json.Unmarshal([]byte(w.Body.String()), &myItems)
+	// put http response result in resBody
+	var resBody map[string][]models.Item
+	err = json.Unmarshal([]byte(w.Body.String()), &resBody)
 	assert.Equal(t, err, nil)
 
-	return myItems["data"]
+	return resBody["data"]
 }
 
 func _update(t *testing.T, router *gin.Engine, token string, updateId string, updateItem dto.UpdateItemInput) models.Item {
 	// update request body
-	updateReqBody, err := json.Marshal(updateItem)
+	reqBody, err := json.Marshal(updateItem)
 	assert.Equal(t, err, nil)
 
 	// update http request
-	updateReq, err := http.NewRequest(http.MethodPut, "/items/"+updateId, bytes.NewBuffer(updateReqBody))
+	req, err := http.NewRequest(http.MethodPut, "/items/"+updateId, bytes.NewBuffer(reqBody))
 	assert.Equal(t, err, nil)
 
 	// set token in header
-	updateReq.Header.Set("Authorization", "Bearer "+token)
+	req.Header.Set("Authorization", "Bearer "+token)
 
 	// http response
 	w := httptest.NewRecorder()
 
 	// do API request
-	router.ServeHTTP(w, updateReq)
+	router.ServeHTTP(w, req)
 
 	// ? update succeeded
 	assert.Equal(t, http.StatusOK, w.Code)
 
-	// updated item
-	var updateRespBody map[string]models.Item
-	err = json.Unmarshal(w.Body.Bytes(), &updateRespBody)
+	// put http response result in resBody
+	var resBody map[string]models.Item
+	err = json.Unmarshal(w.Body.Bytes(), &resBody)
 	assert.Equal(t, err, nil)
 
-	return updateRespBody["data"]
+	return resBody["data"]
 }
 
 func _delete(t *testing.T, router *gin.Engine, token string, deleteId string) {
 	// http request
-	req, _ := http.NewRequest(http.MethodDelete, "/items/"+deleteId, nil)
+	req, err := http.NewRequest(http.MethodDelete, "/items/"+deleteId, nil)
+	assert.Equal(t, err, nil)
+
 	// http response
 	w := httptest.NewRecorder()
+
 	// set Authorization token in header
 	req.Header.Set("Authorization", "Bearer "+token)
+
 	// do API FindMyAll request
 	router.ServeHTTP(w, req)
 
@@ -196,102 +209,98 @@ func _delete(t *testing.T, router *gin.Engine, token string, deleteId string) {
 }
 
 func TestFindMyAll(t *testing.T) {
-	// set up router
-	router := setUp()
+	testRouter := setUp()
 
 	/* 1. Sign up by new user */
-	signUpUser := _signUp(t, router, TestUser)
+	signUpUser := _signUp(t, testRouter, TestUser)
 
 	/* 2. Login */
-	token := _login(t, router, dto.LoginInput{Email: signUpUser.Email, Password: signUpUser.Password})
+	token := _login(t, testRouter, dto.LoginInput{Email: signUpUser.Email, Password: signUpUser.Password})
 
 	/* 3. Create 10 items by login user */
-	newItemIDs := _createTestItems(t, router, token, 10)
+	newItemIDs := _createTestItems(t, testRouter, token, 10)
 
 	/* 4. Test FindMyAll method */
-	myItems := _findMyAll(t, router, token)
+	myItems := _findMyAll(t, testRouter, token)
 
 	/* 5. assert check */
 	assert.Equal(t, len(newItemIDs), len(myItems))
 }
 
 func TestCreate(t *testing.T) {
-	// set up test
-	router := setUp()
+	testRouter := setUp()
 
 	/* 1. Sign up by new user */
-	signUpUser := _signUp(t, router, TestUser)
+	signUpUser := _signUp(t, testRouter, TestUser)
 
 	/* 2. Login */
-	token := _login(t, router, dto.LoginInput{Email: signUpUser.Email, Password: signUpUser.Password})
+	token := _login(t, testRouter, dto.LoginInput{Email: signUpUser.Email, Password: signUpUser.Password})
 
 	/* 3. asset check (not yet created) */
 	// get all items
-	currentItems := _findMyAll(t, router, token)
-	// ? is empty
+	currentItems := _findMyAll(t, testRouter, token)
+	// ? is empty at first
 	assert.Equal(t, 0, len(currentItems))
 
 	/* 4. create 10 items */
-	newItemIDs := _createTestItems(t, router, token, 10)
+	newItemIDs := _createTestItems(t, testRouter, token, 10)
 
 	/* 5. assert check (created 10 items) */
 	assert.Equal(t, len(newItemIDs), 10)
 }
 
 func TestDelete(t *testing.T) {
-	// set up test
-	router := setUp()
+	testRouter := setUp()
 
 	/* 1. Sign up by new user */
-	signUpUser := _signUp(t, router, TestUser)
+	signUpUser := _signUp(t, testRouter, TestUser)
 
 	/* 2. Login */
-	token := _login(t, router, dto.LoginInput{Email: signUpUser.Email, Password: signUpUser.Password})
+	token := _login(t, testRouter, dto.LoginInput{Email: signUpUser.Email, Password: signUpUser.Password})
 
 	/* 3. Create 10 items by login user */
-	newItemIDs := _createTestItems(t, router, token, 10)
+	newItemIDs := _createTestItems(t, testRouter, token, 10)
 
 	/* 4. Delete one item */
 	// delete first index in new items
 	deleteId := strconv.Itoa(int(newItemIDs[0]))
-	_delete(t, router, token, deleteId)
+	_delete(t, testRouter, token, deleteId)
 
 	/* 5. get items */
-	myItems := _findMyAll(t, router, token)
+	myItems := _findMyAll(t, testRouter, token)
 
 	/* 6. assert check */
 	assert.Equal(t, 9, len(myItems))
 }
 
 func TestUpdate(t *testing.T) {
-	/* 1. set up router */
-	router := setUp()
+	testRouter := setUp()
 
-	/* 2. Signup by test user */
-	newUser := _signUp(t, router, TestUser)
+	/* 1. Signup by test user */
+	newUser := _signUp(t, testRouter, TestUser)
 
-	/* 3. Login */
-	token := _login(t, router, dto.LoginInput{Email: newUser.Email, Password: newUser.Password})
+	/* 2. Login */
+	token := _login(t, testRouter, dto.LoginInput{Email: newUser.Email, Password: newUser.Password})
 
-	/* 4. Create 10 items */
-	itemIDs := _createTestItems(t, router, token, 10)
+	/* 3. Create 10 items */
+	itemIDs := _createTestItems(t, testRouter, token, 10)
 
-	/* 5. Update one of the created items */
-	updatedName := "updated name"
-	updatedPrice := uint(9999)
-	updatedDescription := "updated"
+	/* 4. Update one of the created items */
+	updateName := "updated name"
+	updatePrice := uint(9999)
+	updateDescription := "updated"
 
 	updateItem := dto.UpdateItemInput{
-		Name:        &updatedName,
-		Price:       &updatedPrice,
-		Description: &updatedDescription,
+		Name:        &updateName,
+		Price:       &updatePrice,
+		Description: &updateDescription,
 	}
 	// update first index item
 	updateId := strconv.Itoa(int(itemIDs[0]))
-	updatedItem := _update(t, router, token, updateId, updateItem)
+	updatedItem := _update(t, testRouter, token, updateId, updateItem)
 
-	/* 6. assert check */
-	assert.Equal(t, updatedItem.Name, updatedName)
-	assert.Equal(t, updatedItem.Price, updatedPrice)
-	assert.Equal(t, updatedItem.Description, updatedDescription)
+	/* 5. assert check */
+	assert.Equal(t, updatedItem.Name, updateName)
+	assert.Equal(t, updatedItem.Price, updatePrice)
+	assert.Equal(t, updatedItem.Description, updateDescription)
 }
